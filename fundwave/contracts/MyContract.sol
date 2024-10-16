82fed2965b56905aabdf3c9a56e1d145065f8e73
Tirth <55307429+AceAltair13@users.noreply.github.com>
2024-10-16 11:00:08 -0700

    Change contracts name and reformat code

--- fundwave/contracts/MyContract.sol
+++ fundwave/contracts/MyContract.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.9;
 
 contract MyContract {
-
     // Structure to represent a crowdfunding campaign
     struct CrowdFundingCampaign {
         address owner;
@@ -21,12 +20,22 @@ contract MyContract {
 
     // Counter to keep track of the number of campaigns
     uint256 public campaignCount = 0;
-    
-    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _targetAmount, uint256 _deadline, string memory _imageUrl) public returns (uint256){
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _targetAmount,
+        uint256 _deadline,
+        string memory _imageUrl
+    ) public returns (uint256) {
         CrowdFundingCampaign storage newCampaign = campaigns[campaignCount];
 
         // Ensuring the deadline is in the future
-        require(newCampaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+        require(
+            newCampaign.deadline < block.timestamp,
+            "The deadline should be a date in the future."
+        );
 
         // Initializing the new campaign with provided details
         newCampaign.owner = _owner;
@@ -42,7 +51,7 @@ contract MyContract {
         return campaignCount - 1;
     }
 
-     function donateCampaign(uint256 _id) public payable{
+    function donateCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
 
         CrowdFundingCampaign storage campaign = campaigns[_id];
@@ -50,25 +59,33 @@ contract MyContract {
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
 
-        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
 
-        if(sent) {
+        if (sent) {
             campaign.amountCollected = campaign.amountCollected + amount;
-     }
-     }
-
-     function getDonators(uint256 _id) view public returns(address[] memory, uint256[] memory){
-         return (campaigns[_id].donators, campaigns[_id].donations);
-     }
-
-    function getCampaigns() public view returns(CrowdFundingCampaign[] memory){
-        CrowdFundingCampaign[] memory allCampaigns = new CrowdFundingCampaign[](campaignCount);
-
-    for(uint i = 0; i < campaignCount; i++) {
-        CrowdFundingCampaign storage item = campaigns[i];
-
-        allCampaigns[i] = item;
+        }
     }
-       return allCampaigns;
+
+    function getDonators(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns()
+        public
+        view
+        returns (CrowdFundingCampaign[] memory)
+    {
+        CrowdFundingCampaign[] memory allCampaigns = new CrowdFundingCampaign[](
+            campaignCount
+        );
+
+        for (uint i = 0; i < campaignCount; i++) {
+            CrowdFundingCampaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+        return allCampaigns;
+    }
 }
-     }
