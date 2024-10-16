cec3784d7c86209151e693120b067f14d6b0c46b
Tirth <55307429+AceAltair13@users.noreply.github.com>
2024-10-16 13:09:25 -0700

    Add thirdweb to next.js project

---
+++ config.ts
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const CLIENT_ID: string = process.env.CLIENT_ID || '';
+export const CONTRACT_ADDRESS: string = "0xFffa86f41E7861DB8e66D8C1F705eFb990a86d07";
