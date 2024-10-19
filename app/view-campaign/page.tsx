e1efc04b7654684858deaf13c13bf48f5257db17
kunalp10 <pandeykunal2510@gmail.com>
2024-10-19 12:15:19 -0700

    Redirect Changes

--- app/view-campaign/page.tsx
+++ app/view-campaign/page.tsx
@@ -1,19 +1,5 @@
-'use client'
-import { useEffect } from 'react';
+import { redirect } from "next/navigation";
 
-const HomePage = () => {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      // Use replace to redirect without adding to history
-      window.location.replace('/');
-    }
-  }, []);
-
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <p className="text-xl font-bold text-gray-600">Redirecting to Home...</p>
-    </div>
-  );
-};
-
-export default HomePage;
+export default function Page() {
+  redirect("/")
+}
