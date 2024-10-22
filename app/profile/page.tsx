a11d9cd8a448966bc9de9f9ca5d69a2244d89739
Tirth <55307429+AceAltair13@users.noreply.github.com>
2024-10-22 12:07:14 -0700

    Refactor page.tsx: Update campaign skeleton, fetch campaigns, and handle expired session

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import DisplayCampaigns from '@/components/display-campaigns';
-import { Skeleton } from '@/components/ui/skeleton';
+import FetchCampaignSkeleton from '@/components/fetch-campaigns-skeleton';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { contract } from '@/lib/smartcontract';
 import { getParsedCampaigns } from '@/lib/utils';
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
 import { useActiveAccount, useReadContract } from 'thirdweb/react';
 
 
@@ -18,14 +20,23 @@ export default function Profile() {
 
   const DisplayCampaignSkeleton = () => {
     return (
-      <div className="flex flex-col space-y-3">
-        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
-      </div>
+      <>
+        <h1 className="text-xl font-semibold">Fetching Your Campaigns...</h1>
+        <FetchCampaignSkeleton />
+      </>
     );
+  }
+
+  if (!account && !isPending) {
+    return (
+      <Alert variant="destructive">
+        <ExclamationTriangleIcon className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          Your session has expired or you have not logged into a wallet. Please log in again.
+        </AlertDescription>
+      </Alert>
+    )
   }
 
   return (
